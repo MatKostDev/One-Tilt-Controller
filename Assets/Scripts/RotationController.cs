16cad5258966bf477e2c7b190ebe76eab106ee24
MatK <mathew.kostrzewa@ontariotechu.net>
2022-04-01 19:40:15 -0400

    Attempting to get stick rotation reading (broken for now)

--- Assets/Scripts/RotationController.cs
+++ Assets/Scripts/RotationController.cs
@@ -11,30 +11,47 @@ public class RotationController : MonoBehaviour
 	private string vAxisName;
 
 	[SerializeField]
-	private float maxAxisRotation = 50f;
+	private float maxAxisRotation = 30f;
 
 	private void Update()
 	{
-		float hAxis = Input.GetAxisRaw(hAxisName);
+		float hAxis = Input.GetAxis(hAxisName);
+		Debug.Log(hAxis);
 		if (Mathf.Abs(hAxis) > 0.1f)
 		{
-			float multiplier = hAxis > 0f ? 1f : -1f;
+			float multiplier = hAxis > 0f ? -1f : 1f;
 
 			Vector3 newEulers = transform.eulerAngles;
-			newEulers.y = Mathf.Lerp(0f, maxAxisRotation * multiplier, 1f / hAxis);
+			newEulers.y = Mathf.Lerp(0f, maxAxisRotation * multiplier, 1f / Mathf.Abs(hAxis));
 
-			transform.eulerAngles = newEulers;
+			transform.rotation = Quaternion.Euler(newEulers);
+		} 
+		else
+		{
+			Vector3 newEulers = transform.eulerAngles;
+			newEulers.y = 0f;
+
+			transform.rotation = Quaternion.Euler(newEulers);
 		}
 
-		float vAxis = Input.GetAxisRaw(vAxisName);
+		float vAxis = Input.GetAxis(vAxisName);
+		Debug.Log(vAxis);
 		if (Mathf.Abs(vAxis) > 0.1f)
 		{
 			float multiplier = vAxis > 0f ? 1f : -1f;
 
 			Vector3 newEulers = transform.eulerAngles;
-			newEulers.x = Mathf.Lerp(0f, maxAxisRotation * multiplier, 1f / vAxis);
+			newEulers.x = Mathf.Lerp(0f, maxAxisRotation * multiplier, 1f / Mathf.Abs(vAxis));
 
-			transform.eulerAngles = newEulers;
+			transform.rotation = Quaternion.Euler(newEulers);
 		}
+		else
+		{
+			Vector3 newEulers = transform.eulerAngles;
+			newEulers.x = 0f;
+
+			transform.rotation = Quaternion.Euler(newEulers);
+		}
+		Debug.Log("--------");
 	}
 }
